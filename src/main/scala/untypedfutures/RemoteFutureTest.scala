458b14a4cf3d493116fc8074f86498c41eeb6d49
Nikolay Botev <bono8106@yahoo.com>
2011-12-01 20:29:47 -0800

    support for launching only the client or only the server

--- src/main/scala/untypedfutures/RemoteFutureTest.scala
+++ src/main/scala/untypedfutures/RemoteFutureTest.scala
@@ -4,15 +4,24 @@ import akka.actor.Actor
 
 object RemoteFutureTest extends App {
 
-  val service = Actor.actorOf(new ServiceActor).start
+  def startService {
+    val service = Actor.actorOf(new ServiceActor).start
 
-  Actor.remote.start("0.0.0.0", 9999)
-  Actor.remote.register("service", service)
+    Actor.remote.start("0.0.0.0", 9999)
+    Actor.remote.register("service", service)
+  }
 
-  val remoteService = Actor.remote.actorFor("service", "localhost", 9999)
+  def startClient {
+    val remoteService = Actor.remote.actorFor("service", "localhost", 9999)
+    val client = Actor.actorOf(new ClientActor).start
 
-  val client = Actor.actorOf(new ClientActor).start
+    client ! Go(remoteService)
+  }
 
-  client ! Go(remoteService)
+  args match {
+    case Array("--client") => startClient
+    case Array("--server") => startService
+    case _ => startService; startClient
+  }
 
 }
