25d36f18a4d867f614be2072dfa86c65e50aea66
Nikolay Botev <bono8106@yahoo.com>
2011-12-02 13:21:40 -0800

    isolated actors tell vs ask demo

---
+++ src/main/scala/tellorask/IdiomaticActors.scala
@@ -0,0 +1,41 @@
+package tellorask
+
+object IdiomaticActors extends App {
+
+  import akka.actor.Actor
+  import akka.actor.ActorRef
+  import akka.actor.ActorSystem
+  import akka.actor.Props
+
+  lazy val actorSystem = ActorSystem("test")
+
+  def newThreadDispatcher(name: String) = actorSystem.dispatcherFactory.
+    newDispatcher(name).setCorePoolSize(1).setMaxPoolSize(1).build
+
+  def namedThreadActorOf(f: => Actor, name: String) = 
+    actorSystem.actorOf(Props(f).withDispatcher(newThreadDispatcher(name)))
+
+  def log(msg: String) {
+    println("[" + Thread.currentThread.getName + "] " + msg)
+  }
+
+  class ServiceActor extends Actor {
+    def receive = { case x: Int =>
+        log("Service processing " + x)
+        sender ! (x * x)
+    }
+  }
+
+  class ClientActor extends Actor {
+    def receive = {
+      case serviceProvider: ActorRef => serviceProvider ! 42
+      case response: Int => log("Client got back " + response)
+    }
+  }
+
+  val service = namedThreadActorOf(new ServiceActor, "service")
+  val client = namedThreadActorOf(new ClientActor, "client")
+
+  client ! service
+
+}
