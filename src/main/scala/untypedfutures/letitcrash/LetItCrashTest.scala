f743edf5a7048485e950ceb9cf9672652077e59a
Nikolay Botev <bono8106@yahoo.com>
2011-12-11 21:08:30 -0800

    simplify

--- src/main/scala/untypedfutures/letitcrash/LetItCrashTest.scala
+++ src/main/scala/untypedfutures/letitcrash/LetItCrashTest.scala
@@ -2,45 +2,52 @@ package untypedfutures.letitcrash
 
 import akka.actor.Actor
 import akka.actor.ActorRef
-import untypedfutures.{namedThreadActorOf, log}
+import untypedfutures.{actorSystem, namedThreadActorOf, log}
 
 object LetItCrashTest extends App {
 
+  case class Request(i: Int)
+  case class Response(j: Int)
   case object Fail
 
-  class ServiceActor extends Actor {
+  class Executive extends Actor {
+    def receive = {
+      case Request(i) =>
+        log("processing " + i)
+        Thread.sleep(100) // Do some work ..
+        log("processed " + i)
+        sender ! Response(i*i)
+    }
+  }
 
-    var n = 0
+  class Director extends Actor {
+
+    val exec = namedThreadActorOf(new Executive, "executive")
+
+    /**
+     * Counts requests submitted to processor and pending completion.
+     */
+    var counter = 0
 
     def receive = {
-      case i: Int =>
-        log("service " + n + " - " + i)
-        n += 1
+      case request @ Request(i) =>
+        exec ! request
+        counter += 1
+        log("processing " + i + "; pending request counter = " + counter)
+      case Response(j) =>
+        counter -= 1
+        log("processed " + j + "; pending request counter = " + counter)
       case Fail =>
-        Thread.sleep(1000) // let mailbox fill up
+        Thread.sleep(1000) // let my mailbox accumulate some messages
         throw new RuntimeException("Service actor failing.")
     }
 
   }
 
-  class ClientActor extends Actor {
+  val director = namedThreadActorOf(new Director, "director")
 
-    def receive = {
-      case s: ActorRef =>
-        for (i <- 1 to 10) {
-          s ! i
-        }
-        s ! Fail
-        for (i <- 11 to 20) {
-          s ! i
-        }
-    }
-
-  }
-
-  val service = namedThreadActorOf(new ServiceActor, "service")
-  val client = namedThreadActorOf(new ClientActor, "client")
-
-  client ! service
+  director ! Request(1)
+  director ! Fail
+  director ! Request(2)
 
 }
