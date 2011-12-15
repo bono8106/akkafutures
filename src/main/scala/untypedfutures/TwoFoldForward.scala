3773bdb71345a55aa2073075beaf090dc259fa22
Nikolay Botev <bono8106@yahoo.com>
2011-12-15 03:56:16 -0800

    initial twofoldforward

---
+++ src/main/scala/untypedfutures/TwoFoldForward.scala
@@ -0,0 +1,53 @@
+package untypedfutures
+
+import akka.actor.Actor
+
+object TwoFoldForward extends App {
+
+  case object Go
+
+  class A extends Actor {
+
+    val b = namedThreadActorOf(new B, "B")
+
+    def receive = {
+      case Go =>
+        val f = b ? "hello"
+        f onResult { case _ => log("resolved to " + f.get) }
+        f onException { case e => log("smashed with " + e) }
+    }
+  }
+
+  class B extends Actor {
+
+    val c = namedThreadActorOf(new C, "C")
+    val d = namedThreadActorOf(new D, "D")
+
+    def receive = {
+      case m: String =>
+        c forward m
+        d forward m
+    }
+
+  }
+
+  class C extends Actor {
+    def receive = {
+      case m: String =>
+        Thread.sleep(200)
+        sender ! m + " world"
+    }
+  }
+
+  class D extends Actor {
+    def receive = {
+      case m: String =>
+        Thread.sleep(100)
+        throw new Exception("error")
+    }
+  }
+
+  val a = namedThreadActorOf(new A, "A")
+  a ! Go
+
+}
