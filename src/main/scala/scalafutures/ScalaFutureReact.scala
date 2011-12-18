c3c50e3a2e723a9a07abe5e1d812b9d603ed6c42
Nikolay Botev <bono8106@yahoo.com>
2011-12-18 00:30:22 -0800

    blocking receive in scala is exposed

--- src/main/scala/scalafutures/ScalaFutureReact.scala
+++ src/main/scala/scalafutures/ScalaFutureReact.scala
@@ -25,23 +25,26 @@ object ScalaFutureReact extends App {
         log("Client enter " + self)
         val future = serviceProvider !! 42;
         self mkBody {
+          self ! "cannot interleave with blocking receive on future"
+          log("Entering blockiing receive...")
           future.inputChannel react {
             case response: Int =>
               log("Client got future result " + response)
               self ! response
-              Thread.sleep(1000)
+              Thread.sleep(3000)
               log("Client future react done")
           }
         } andThen eventloop(handler)
         log("Client leave") // never reached
       case response: Int =>
         log("Client got back " + response)
+      case message: String =>
+        log("Message: " + message)
     }
 
     eventloop(handler)
   }
 
-  client ! service
   client ! service
 
 }
