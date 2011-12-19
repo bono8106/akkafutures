5e06da442ab01320e80229773173e475a261473d
Nikolay Botev <bono8106@yahoo.com>
2011-12-19 01:41:02 -0800

    let-it-crash for typed actors

---
+++ src/main/scala/typedfutures/LetItCrashOrPipe.scala
@@ -0,0 +1,33 @@
+package typedfutures
+
+import akka.actor._
+import akka.dispatch.Future
+
+object LetItCrashOrPipe extends App {
+
+  trait PipeOrCrash {
+    def pipe: Future[Any]
+    def crash: Unit
+
+    def go: Unit
+  }
+
+  class PipeOrCrashActor extends PipeOrCrash {
+    def pipe: Future[Any] = { println("this="+this); throw new Exception("Exception piped through future") }
+    def crash: Unit = { println("this="+this); throw new Exception("Exception piped to supervisor (let-it-crash)") }
+    def go {
+      val self = TypedActor.self[PipeOrCrash]
+      self.pipe onComplete { result => println("Future completed with " + result) }
+      self.crash
+    }
+
+  }
+
+  val actorSystem = ActorSystem()
+  val typedActors = TypedActor(actorSystem)
+
+  val actor = typedActors.typedActorOf(classOf[PipeOrCrash], classOf[PipeOrCrashActor], Props())
+
+  actor.go
+
+}
